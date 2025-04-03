85840c4ef82a5f2fc84b001be771eb28884a3415
Harish Saud <saudharish770@gmail.com>
2025-04-03 21:02:38 +0545

    Add files via upload
    python mac changer code which need sudo permission for use.

---
+++ machanger.py
@@ -0,0 +1,22 @@
+import os
+import random
+import re
+
+def random_mac():
+    return "02:%02x:%02x:%02x:%02x:%02x" % (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+    )
+
+def change_mac(interface, new_mac):
+    os.system(f"sudo ifconfig {interface} down")
+    os.system(f"sudo ifconfig {interface} hw ether {new_mac}")
+    os.system(f"sudo ifconfig {interface} up")
+    print(f"[+] MAC Address changed to: {new_mac}")
+
+interface = input("Enter network interface (e.g., eth0, wlan0): ")
+new_mac = random_mac()
+change_mac(interface, new_mac)
